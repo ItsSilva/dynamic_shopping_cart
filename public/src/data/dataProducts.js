2b993c36d9dbb603fab4909f5993183a54489de6
Juan Silva <juanse909@hotmail.com>
2024-09-13 11:16:50 -0500

    data build

---
+++ public/src/data/dataProducts.js
@@ -0,0 +1,8 @@
+const dataProducts = [
+    { id: 1, name: "Laptop", price: 1500, quantity: 10 },
+    { id: 2, name: "Smartphone", price: 800, quantity: 15 },
+    { id: 3, name: "Headphones", price: 200, quantity: 50 },
+    { id: 4, name: "Keyboard", price: 100, quantity: 20 },
+    { id: 5, name: "Monitor", price: 300, quantity: 8 },
+  ];
+export default dataProducts;
