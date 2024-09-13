9b0cc616f3effa37e11b6b83a995c955d3f159af
Juan Silva <juanse909@hotmail.com>
2024-09-13 11:15:56 -0500

    index father

---
+++ public/src/components/index.js
@@ -0,0 +1,3 @@
+export {default as ProductList} from './productList/productList.js'
+export {default as ProductItem} from './productItem/productItem.js'
+export {default as ShoppingCart} from './ShoppingCart/ShoppingCart.js'
